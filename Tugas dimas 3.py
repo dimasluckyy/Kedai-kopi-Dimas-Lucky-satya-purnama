c3bf1961b8e217c0536f9d878960a6f865b818f5
dimasluckyy <135620697+dimasluckyy@users.noreply.github.com>
2023-06-06 15:22:01 +0700

    Update Tugas dimas 3.py

--- Tugas dimas 3.py
+++ Tugas dimas 3.py
@@ -1,26 +1,51 @@
-class MenuKopi:
-    def __init__(self, nama, harga):
-        self.__nama = nama
-        self.__harga = harga
-
-    def get_nama(self):
-        return self.__nama
-
-    def get_harga(self):
-        return self.__harga
-
-
-class AnandaCoffee:
+class Discount:
     def __init__(self):
-        self.__menu_kopi = [
-            MenuKopi("ES Kopi Susu", 11000),
-            MenuKopi("ES Kopi Coklat", 12000),
-            MenuKopi("ES Kopi Hitam", 11000),
-            MenuKopi("Ice Americano", 14000)
-        ]
-        self.__diskon = Diskon()
+        self.discount_levels = {
+            1: 0.05,   # 5% discount
+            2: 0.1,    # 10% discount
+            3: 0.15,   # 15% discount
+            4: 0.2,    # 20% discount
+            5: 0.25    # 25% discount
+        }
 
-    def tampilkan_menu(self):
+    def calculate_discount(self, quantity):
+        if quantity >= 5:
+            return self.discount_levels[5]
+        elif quantity >= 4:
+            return self.discount_levels[4]
+        elif quantity >= 3:
+            return self.discount_levels[3]
+        elif quantity >= 2:
+            return self.discount_levels[2]
+        elif quantity >= 1:
+            return self.discount_levels[1]
+        else:
+            return 0
+
+
+class CoffeeMenu:
+    def __init__(self):
+        self.menu = {
+            "A": {"name": "ES Kopi Susu", "price": 11000},
+            "B": {"name": "ES Kopi Coklat", "price": 12000},
+            "C": {"name": "ES Kopi Hitam", "price": 11000},
+            "D": {"name": "Ice Americano", "price": 14000}
+        }
+
+    def calculate_total_price(self, menu_choice, quantity):
+        item = self.menu.get(menu_choice.upper())
+        if item:
+            price = item["price"] * quantity
+            discount_percentage = Discount().calculate_discount(quantity)
+            discount = int(price * discount_percentage)
+            subtotal = price - discount
+            tax = int(subtotal * 0.1)
+            total_price = subtotal + tax
+            return item["name"], price, discount, tax, total_price
+        else:
+            return "-", "-", "-", "-", "-"
+
+    def display_menu(self):
         print("""
         ==============================
         
@@ -35,69 +60,31 @@ class AnandaCoffee:
         ==============================
         """)
 
-    def pesan_menu(self):
-        pesan = input("Masukkan list abjad menu kopi = ").lower()
-        jumlah_pesan = int(input("Masukkan jumlah pesanan = "))
-
-        menu = self.__get_menu(pesan)
-        if menu is not None:
-            nama = menu.get_nama()
-            harga = menu.get_harga() * jumlah_pesan
-            ppn = int(harga * 0.1)
-
-            diskon = self.__diskon.get_diskon(jumlah_pesan)
-            total_harga = harga - diskon + ppn
-
-            print("--------------------------")
-            print("Ananda Coffee")
-            print("--------------------------")
-            print("Menu :", nama)
-            print("Jumlah Pesan :", jumlah_pesan)
-            print("Harga :", harga)
-            print("Diskon :", diskon)
-            print("PPN :", ppn)
-            print("--------------------------")
-            print("Jumlah Bayar :", total_harga)
-            print("--------------------------")
-        else:
-            print("Menu tidak tersedia.")
-
-    def __get_menu(self, pesan):
-        for menu in self.__menu_kopi:
-            if pesan == menu.get_nama().lower():
-                return menu
-        return None
+    def display_order(self, menu_choice, quantity, name, price, discount, tax, total_price):
+        print("--------------------------")
+        print("Ananda Coffee")
+        print("--------------------------")
+        print("Menu:", name)
+        print("Jumlah Pesan:", quantity)
+        print("Harga:", price)
+        print("Diskon:", discount)
+        print("PPN:", tax)
+        print("--------------------------")
+        print("Jumlah Bayar:", total_price)
+        print("--------------------------")
 
 
-class Diskon:
-    def __init__(self):
-        self.__tingkatan_diskon = {
-            1: 0.05,  # 5% diskon
-            2: 0.1,   # 10% diskon
-            3: 0.15,  # 15% diskon
-            4: 0.2,   # 20% diskon
-            5: 0.25   # 25% diskon
-        }
-
-    def get_diskon(self, jumlah_pesan):
-        if jumlah_pesan >= 5:
-            return int(jumlah_pesan * self.__tingkatan_diskon[5])
-        elif jumlah_pesan >= 4:
-            return int(jumlah_pesan * self.__tingkatan_diskon[4])
-        elif jumlah_pesan >= 3:
-            return int(jumlah_pesan * self.__tingkatan_diskon[3])
-        elif jumlah_pesan >= 2:
-            return int(jumlah_pesan * self.__tingkatan_diskon[2])
-        elif jumlah_pesan >= 1:
-            return int(jumlah_pesan * self.__tingkatan_diskon[1])
-        else:
-            return 0
+def main():
+    coffee_menu = CoffeeMenu()
+    choice = "Y"
+    while choice == "Y":
+        coffee_menu.display_menu()
+        menu_choice = input("Masukkan list abjad menu kopi: ")
+        quantity = int(input("Masukkan jumlah pesanan: "))
+        name, price, discount, tax, total_price = coffee_menu.calculate_total_price(menu_choice, quantity)
+        coffee_menu.display_order(menu_choice, quantity, name, price, discount, tax, total_price)
+        choice = input("Apakah Anda ingin order kembali? (Y/N): ")
 
 
-pilihan = "iya"
-ananda_coffee = AnandaCoffee()
-
-while pilihan == "iya":
-    ananda_coffee.tampilkan_menu()
-    ananda_coffee.pesan_menu()
-    pilihan = input("Apakah Anda ingin order kembali? (iya/tidak) = ").lower()
+if __name__ == "__main__":
+    main()
